fd0816e48024353e63d5cbcd464bae8f27e34915
Giovanna <giovannacostaxavier@gmail.com>
2026-07-30 08:25:37 +0100

    atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ const App =()=>{
    
   return(
     <div>
-     { /*<Aluno nome = "Giovanna" curso="React">
+     {/*<Aluno nome = "Giovanna" curso="React">
       </Aluno>
       <Cartao nome = "Giovanna" cidade="Porto" profissao="Desenvolvedora">
       </Cartao>*/}
+      
       
 
     </div>
